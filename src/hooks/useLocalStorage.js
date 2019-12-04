ecc08508b77978f725548c0a2f09b4b22addb690
lukasjaronis <jaronisluke13@gmail.com>
2019-12-04 16:50:12 -0500

    localstorage done

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -2,5 +2,15 @@ import { useState } from 'react';
 
 export const useLocalStorage = (key, initialValue) => {
 
-    return 
-}
+    const originalValue = localStorage.getItem(key);
+    console.log("original", originalValue);
+
+    const [storedValue, setStoredValue] = useState(initialValue);
+
+    const setValue = value => {
+        localStorage.setItem(key, Boolean(value));
+        setStoredValue(Boolean(value));
+    }
+
+    return [storedValue, setValue];
+};
